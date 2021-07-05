ef56779a54c5c9c3a9e78bcc94235fa6df71c6a3
zhoudexiao <zhoudexiao@58.com>
2021-07-05 23:06:40 +0800

    updateRotateArray

--- basic/src/primary/array/RotateArray.java
+++ basic/src/primary/array/RotateArray.java
@@ -9,10 +9,10 @@ import java.util.Arrays;
 public class RotateArray {
 
     public static void main(String[] args){
-        int nums[] = new int[]{1, 2, 3, 4,6,8};
-        int k = 2;
+        int nums[] = new int[]{1, 2, 3, 4, 6, 8};
+        int k = 7;
         System.out.println("premArray-" + Arrays.toString(nums));
-        rotate_Round(nums, k);
+        rotate_Reverse(nums, k);
         System.out.println("rotateArray-" + Arrays.toString(nums));
     }
 
@@ -50,15 +50,6 @@ public class RotateArray {
 
     }
 
-    public static void reverse(int[] nums, int index, int n, int k){
-        int nextIndex = index + n * k;
-        if(nextIndex < nums.length){
-            int tempValue = nums[index];
-            nums[index] = nums[nextIndex];
-            nums[nextIndex] = tempValue;
-            reverse(nums, index, ++n, k);
-        }
-    }
 
     public static void rotate_NewArray(int[] nums, int k) {
         int n = nums.length;
@@ -78,14 +69,59 @@ public class RotateArray {
             index = (index + k) % length;
             if (visited[index]) {
                 index = (index + 1) % length;
-                hold = nums[index];
-                i--;
-            } else {
-                visited[index] = true;
-                int temp = nums[index];
-                nums[index] = hold;
-                hold = temp;
+                hold = nums[(index + length - k) % length];
             }
+            visited[index] = true;
+            int temp = nums[index];
+            nums[index] = hold;
+            hold = temp;
+        }
+    }
+
+    public static void rotate_Pro(int[] nums, int k) {
+        if (k < 1 || nums.length <= 1) {
+            return;
+        }
+        if(nums.length % k == 0){
+            for (int i = 0; i < k; i++) {
+                reverse(nums, i, 1, k);
+            }
+            return;
+        }
+        int length = nums.length;
+        int hold = nums[0];
+        int nextIndex = 0;
+        for (int j = 0; j < length; j++) {
+            nextIndex = (nextIndex + k) % length;
+            int tempt = nums[nextIndex];
+            nums[nextIndex] = hold;
+            hold = tempt;
+        }
+    }
+
+    public static void reverse(int[] nums, int index, int n, int k){
+        int nextIndex = index + n * k;
+        if(nextIndex < nums.length){
+            int tempValue = nums[index];
+            nums[index] = nums[nextIndex];
+            nums[nextIndex] = tempValue;
+            reverse(nums, index, ++n, k);
+        }
+    }
+
+    public static void rotate_Reverse(int[] nums, int k) {
+        int length = nums.length;
+        k = k % length;
+        reverseNum(nums, 0, length - 1);
+        reverseNum(nums, 0, k - 1);
+        reverseNum(nums, k, length - 1);
+    }
+
+    public static void reverseNum(int[] nums, int left, int right){
+        while (left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
         }
     }
 
