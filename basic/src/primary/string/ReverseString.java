cce23746034f9cb664f2fc5198711c2b23f1b9c0
zhoudexiao <zhoudexiao@58.com>
2021-07-05 09:22:14 +0800

    update ReverseString

--- basic/src/primary/string/ReverseString.java
+++ basic/src/primary/string/ReverseString.java
@@ -7,20 +7,27 @@ public class ReverseString {
     public static void main(String[] args) {
         char[] temptArray = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'};
         System.out.println("beforeReverse: " + Arrays.toString(temptArray));
-        reverseString(temptArray);
+        reverseStringA(temptArray);
         System.out.println("afterReverse: " + Arrays.toString(temptArray));
     }
 
     public static void reverseString(char[] s) {
-        if (s == null || s.length == 0) {
-            return;
-        }
         int length = s.length;
         for (int index = 0; index < length / 2; index++) {
             char tempt = s[index];
             int backIndex = length - index - 1;
             s[index] = s[backIndex];
             s[backIndex] = tempt;
+        }
+    }
+
+    public static void reverseStringA(char[] s) {
+        int left = 0;
+        int right = s.length -1;
+        while (left < right){
+            char tempt = s[left];
+            s[left++] = s[right];
+            s[right--] = tempt;
         }
     }
 
